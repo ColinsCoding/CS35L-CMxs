2c0b2282fe2c9f377f62e26bca69f8d536199c53
marcokfung <47341733+marcokfung@users.noreply.github.com>
2023-11-28 15:23:32 -0800

    Removed User Schema for Login Branch Merge

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import { Post } from '../models/pixelArt.js';
-import { User } from '../models/user.js';
 
 const router = express.Router();
 
@@ -100,108 +99,6 @@ router.delete('/posts/:id', async (request, response) => {
         }
 
         return response.status(200).send({ message: 'Post deleted successfully' });
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({ message: error.message });
-    }
-});
-
-
-router.post('/user', async (request, response) => {
-    try {
-        if (
-            !request.body.username || 
-            !request.body.password ||
-            !request.body.posts
-        ) {
-            return response.status(400).send({
-                message: 'Must send all fields',
-            });
-        }
-        const newUser = {
-            username: request.body.username,
-            password: request.body.password,
-            posts: request.body.posts,
-        };
-
-        const user = await User.create(newUser);
-
-        return response.status(201).send(user)
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({ message: error.message });
-    }
-});
-
-// Get all users
-router.get('/user', async (request, response) => {
-    try {
-        const users = await User.find({});
-
-        return response.status(200).json({
-            count: users.length,
-            data: users
-        })
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({ message: error.message });
-    }
-});
-
-// Get one user 
-router.get('/user/:id', async (request, response) => {
-    try {
-
-        const { id } = request.params;
-
-        const user = await User.findById(id);
-
-        return response.status(200).json(user);
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({ message: error.message });
-    }
-});
-
-// Update a user
-router.put('/user/:id', async (request, response) => {
-    try {
-        if (
-            !request.body.username || 
-            !request.body.password ||
-            !request.body.posts
-        ) {
-            return response.status(400).send({
-                message: 'Must send all fields',
-            });
-        }
-
-        const { id } = request.params;
-
-        const result = await User.findByIdAndUpdate(id, request.body);
-
-        if (!result) {
-            return response.status(404).json({ message: 'User not found' });
-        }
-
-        return response.status(200).send({ message: 'User updated successfully' });
-
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({ message: error.message });
-    }
-});
-
-router.delete('/user/:id', async (request, response) => {
-    try {
-        const { id } = request.params;
-        const result = await User.findByIdAndDelete(id);
-
-        if (!result) {
-            return response.status(404).json({ message: 'User not found' });
-        }
-
-        return response.status(200).send({ message: 'User deleted successfully' });
     } catch (error) {
         console.log(error.message);
         response.status(500).send({ message: error.message });
